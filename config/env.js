705e9cec227d3a5107635e0c87dc63f1670aa0ee
ACDevOps23 <135799409+ACDevOps23@users.noreply.github.com>
2025-06-10 11:13:42 +1000

    Update env.js

--- config/env.js
+++ config/env.js
@@ -1,7 +1,5 @@
 import  { config } from "dotenv";
 
-config({path: `.env.${process.env.NODE_ENV || "development"}.local`});
-
 export const { 
     PORT,
     NODE_ENV,
@@ -14,6 +12,4 @@ export const {
     QSTASH_TOKEN,
     QSTASH_URL,
     EMAIL_PASSWORD,
-    // QSTASH_CURRENT_SIGNING_KEY,
-    // QSTASH_NEXT_SIGNING_KEY
 } = process.env;
